f74a26acc79cec8af2db0f60b300539490e68039
heoun <ibyso100@gmail.com>
2026-07-14 17:20:41 +0900

    파이썬 기본 문법1

---
+++ ch1/basic.py
@@ -0,0 +1,24 @@
+# 자료형
+# 숫자형, 문자형
+
+# 1. 숫자형 - 정수, 실수, 8진수, 16진수
+a = 123
+print(f"양의 정수 : {a}")
+
+a = -178
+print(f"음의 정수 : {a}")
+
+a = 1.2
+print(f"양의 실수 : {a}")
+
+a = -3.45
+print(f"음의 실수 : {a}")
+
+multiline = """life is too short,\nYou need Python"""
+print(multiline)
+
+multiline = """
+life is too short,
+You need Python
+"""
+print(multiline)
